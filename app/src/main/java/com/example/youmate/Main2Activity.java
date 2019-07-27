0299e3e42c72ce139350a8041134cd27997a93d4
sufwan <sufwan0001@gmail.com>
2019-07-27 15:11:48 +0500

    copyurl

--- app/src/main/java/com/example/youmate/Main2Activity.java
+++ app/src/main/java/com/example/youmate/Main2Activity.java
@@ -109,7 +109,7 @@ public class Main2Activity extends AppCompatActivity  {
                 Log.d("Succes", "Value is: " + value);
                 CHANNLE_GET_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&channelId="+value+"&maxResults=20&key="+GOOGLE_YOUTUBE_API_KEY+"";
                 initList(mListData);
-                new RequestYoutubeAPI().execute();
+              //  new RequestYoutubeAPI().execute();
             }
 
             @Override
